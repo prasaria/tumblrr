89462570006af587a53bf7a0ef582b25eebdeb3d
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2017-10-27 02:27:43 +0000

    Add Post Index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
     
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
+   
     def index
+      @post = Post.all.order('created_at DESC')
     end
     
     def new
@@ -14,7 +17,23 @@ class PostsController < ApplicationController
         redirect_to @post
     end
     
+    def show
+    end
+    
+    def edit 
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
     private
+    
+    def find_post
+        @post = Post.find(params[:id])
+    end
     
     def post_params
        params.require(:post).permit(:title, :body)    
